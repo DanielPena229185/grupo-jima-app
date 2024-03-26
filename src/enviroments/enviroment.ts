0addc3e628ef15dba578a10a4523643012ad1ddd
DanielPenaGarcia <daniel.pena229185@potros.itson.edu.mx>
2024-03-26 11:26:57 -0700

    Update enviroment.ts
    Lo cambié al localhost la variable api

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const enviroment = {
     icons: '/assets/icons',
     imagenes: '/assets/imagenes',
-    apiUrl: "https://7bl4v3wp-3000.usw3.devtunnels.ms",
+    apiUrl: "http://localhost:3000",
     tortilleriaId: '1',
 }
